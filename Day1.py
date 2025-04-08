673a2b37ec53b5c6931709287afc7fa6bbbe1d49
Ali <muneeb.ali@atco.com>
2025-04-08 13:11:06 -0600

    fix(Day1.py): follow professional var/func naming conventions

--- Day1.py
+++ Day1.py
@@ -1,20 +1,20 @@
-def BandNameGenerator(cityName: str, petName: str) -> str:
+def band_name_generator(city_name: str, pet_name: str) -> str:
     '''
     Function concatenates 2 variables together to give a unique name
     '''
-    bandName: str = cityName + " " + petName
-    return bandName
+    band_name: str = city_name + " " + pet_name
+    return band_name
 
-def main():
+def main() -> None:
     '''
     Main function that runs program
     '''
     print("Welcome to the Band Name Generator")
     try:
-        cityName = input("What's the name of the city you grew up in:\n")
-        petName = input("What's your pet's name?\n")
-        bandName = BandNameGenerator(cityName, petName)
-        print(f"Your band name could be {bandName}!")
+        city_name = input("What's the name of the city you grew up in:\n")
+        pet_name = input("What's your pet's name?\n")
+        band_name = band_name_generator(city_name, pet_name)
+        print(f"Your band name could be {band_name}!")
     except (KeyboardInterrupt, EOFError):
         print("\Program interrupted by user. Goodbye!")
 
