b73121373af7a4dfa01613d3bb3aa8c451bfc507
Ali <muneeb.ali@atco.com>
2025-04-08 14:24:22 -0600

    feat(tip_calculator): add robust error handling/raise exceptions

--- Day2.py
+++ Day2.py
@@ -1,7 +1,14 @@
-def tip_calcluator(total_bill: float, tip_received: float, split_people_count: float) -> float:
+def tip_calculator(total_bill: float, tip_received: float, split_people_count: float) -> float:
     '''
     Function to calculate the cost per head with tip
     '''
+    if total_bill < 0 or tip_received < 0:
+        raise Exception("Bill and tip amounts cannot be negative!")
+    if tip_received not in [10, 12, 15]:
+        raise Exception("Tip must be $10, $12, or $15!")
+    if split_people_count < 0:
+        raise Exception("Number of people must be postive!")
+    
     combined_bill: float = total_bill + tip_received
     new_bill_per_person: float = (combined_bill/split_people_count)
     return new_bill_per_person
@@ -15,10 +22,11 @@ def main() -> None:
         total_bill = float(input("What was your total bill?\nBill amount($): "))
         tip_received = float(input("How much tip would you like to give?\nChoose $10, $12, or $15\nTip amount($): "))
         split_bill_count = float(input("How many people to split the bill?\nPeople count: "))
-        final_amount_per_person = tip_calcluator(total_bill, tip_received, split_bill_count)
+
+        final_amount_per_person = tip_calculator(total_bill, tip_received, split_bill_count)
         print(f"Each person should pay: ${final_amount_per_person:.2f}")
     except (KeyboardInterrupt, EOFError):
-        print("Program interrupted by user. Goodbye!")
+        print("\nProgram interrupted by user. Goodbye!")
 
 if __name__ == "__main__":
     main()
