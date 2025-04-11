77854dedceca5c1d58e585628b90590ca288f13c
muneebalichishti01 <muneeb.ali1@ucalgary.ca>
2025-04-11 09:47:00 -0600

    feat(WORD_LIST): extend the word list to give multiple options

---
+++ Day8.py
@@ -0,0 +1,14 @@
+def feature1():
+    '''
+    Function to implement a small feature
+    '''
+    pass
+
+def main() -> None:
+    '''
+    Main function to run the program
+    '''
+    pass
+
+if __name__ == "__main__":
+    main()
