36afcebf3988c2135eb392523bea5e9089822dad
Muneeb Ali <69442174+muneebalichishti01@users.noreply.github.com>
2025-04-14 05:14:57 -0600

    feat(Day11.py): implement full capstone project of blackjack game

---
+++ Day11.py
@@ -0,0 +1,130 @@
+import random
+
+# Define the constants
+BLACKJACK_ASCII = '''
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _' |/ __| |/ / |/ _' |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+'-----| \  / |     |_.__/|_|\__,_|\___|_|\_| |\__,_|\___|_|\_/
+      |  \/ K|                            _/ |                
+      '------'                           |__/           
+'''
+
+def get_random_cards_list(number: int) -> list:
+    '''
+    Function to generate a list of random numbers based on the number parameter from 1 to 11
+    '''
+    random_num_list = [random.randint(1, 11) for _ in range(number)]
+    return random_num_list
+
+def evaluate_winner(game_cards: dict) -> None:
+    '''
+    Function to evaluate the final winner
+    '''
+    user_score = game_cards["user"]["total_score"]
+    computer_score = game_cards["computer"]["total_score"]
+
+    if user_score > 21:
+        print("You went over 21. You lose!")
+    elif computer_score > 21:
+        print("Computer went over 21. You win!")
+    elif user_score > computer_score:
+        print("You win!")
+    elif user_score < computer_score:
+        print("You lose!")
+    else:
+        print("It's a draw!")
+
+def get_user_next_choice(game_cards: dict) -> None:
+    '''
+    Function to get user's met choice with valid checks
+    '''
+    while True:
+        user_choice = input("Type 'y' to get another card, type 'n' to pass.\nChoice: ").lower()
+        if user_choice == 'n':
+            while game_cards["computer"]["total_score"] < 17:
+                game_cards["computer"]["current_cards"].extend(get_random_cards_list(1))
+                game_cards["computer"]["total_score"] = add_cards(game_cards["computer"]["current_cards"])
+            print(f"    Your final hand: {game_cards['user']['current_cards']}, current score: {game_cards['user']['total_score']}")
+            print(f"    Computer's final hand: {game_cards['computer']['current_cards']}, current score: {game_cards['computer']['total_score']}")
+            evaluate_winner(game_cards)
+            break
+        elif user_choice == 'y':
+            game_cards["user"]["current_cards"].extend(get_random_cards_list(1))
+            game_cards["user"]["total_score"] = add_cards(game_cards["user"]["current_cards"])
+            print(f"    Your cards: {game_cards['user']['current_cards']}, current score: {game_cards['user']['total_score']}")
+            print(f"    Computer's first card: {game_cards['computer']['current_cards'][0]}")
+
+            if game_cards["user"]["total_score"] > 21:
+                evaluate_winner(game_cards)
+                print(f"    Your final hand: {game_cards['user']['current_cards']}, current score: {game_cards['user']['total_score']}")
+                print(f"    Computer's final hand: {game_cards['computer']['current_cards']}, current score: {game_cards['computer']['total_score']}")
+                break
+        else:
+            print("Error: Please type 'y' or 'n'.")
+        
+def add_cards(card_list: list) -> int:
+    '''
+    Function to calculate the final total of all cards in the list without using built-in sum
+    '''
+    card_total = 0
+    for card in card_list:
+        card_total += card
+    return card_total
+
+def play_blackjack() -> None:
+    '''
+    Function to implement play logic of the game
+    '''
+    print(BLACKJACK_ASCII)
+    
+    game_cards = {
+        "user": {
+            "current_cards" : [],
+            "total_score": 0
+        },
+        "computer": {
+            "current_cards" : [],
+            "total_score": 0
+        }
+    }
+
+    game_cards["user"]["current_cards"].extend(get_random_cards_list(2))
+    game_cards["user"]["total_score"] = add_cards(game_cards["user"]["current_cards"])
+    game_cards["computer"]["current_cards"].extend(get_random_cards_list(1))
+    game_cards["computer"]["total_score"] = add_cards(game_cards["computer"]["current_cards"])
+    
+    print(f"    Your cards: {game_cards['user']['current_cards']}, current score: {game_cards['user']['total_score']}")
+    print(f"Computer's first card: {game_cards['computer']['current_cards'][0]}")
+    get_user_next_choice(game_cards)
+
+
+def run_blackjack() -> None:
+    '''
+    Function to implement main black jack logic
+    '''
+    while True:
+        user_input = input("Do you want to play a game of Blackjack? Type 'y' or 'n'.\nChoice: ").lower()
+        if user_input == 'n':
+            print("You chose to quit to the program. Goodbye!")
+            break
+        elif user_input == 'y':
+            play_blackjack()
+        else:
+            print("Error: Please type 'y' or 'n'.")
+
+def main() -> None:
+    '''
+    Main function to run the program
+    '''
+    try:
+        run_blackjack()
+    except (KeyboardInterrupt, EOFError):
+        print("\nProgram interrupted by the user. Goodbye!")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
